e829ae36b7864a53e3ad29310992ef9ff1b708bf
bar2sek-mobile <rbar2sek@icloud.com>
2017-06-09 09:02:18 -0500

    Test for any string input is returned passes.

--- test/AcceptCoinsTest.java
+++ test/AcceptCoinsTest.java
@@ -4,9 +4,16 @@ import static junit.framework.TestCase.*;
 
 public class AcceptCoinsTest {
     @Test
-    public void machineAcceptsOneCoinWhenInserted() {
+    public void machineAcceptsAnyCoinWhenInserted() {
         VendingMachine machine = new VendingMachine();
 
         assertEquals("dime", machine.getCoinsInserted("dime"));
+    }
+
+    @Test
+    public void machineAcceptsNickelWhenInserted() {
+        VendingMachine machine = new VendingMachine();
+
+        assertEquals("nickel", machine.getCoinsInserted("nickel"));
     }
 }
