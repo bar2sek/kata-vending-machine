ba9a88e9a53b4ec5a57195a52d6403b4d1b338e8
bar2sek-mobile <rbar2sek@icloud.com>
2017-06-09 09:27:58 -0500

    Refactor object instantiation into @Before method

--- test/AcceptCoinsTest.java
+++ test/AcceptCoinsTest.java
@@ -1,19 +1,27 @@
+import org.junit.Before;
 import org.junit.Test;
 
 import static junit.framework.TestCase.*;
 
 public class AcceptCoinsTest {
+    private VendingMachine machine;
+
+    @Before
+    public void setUp() throws Exception {
+        machine = new VendingMachine();
+    }
+
     @Test
     public void machineAcceptsAnyCoinWhenInserted() {
-        VendingMachine machine = new VendingMachine();
 
         assertEquals("dime", machine.getCoinsInserted("dime"));
     }
 
     @Test
     public void machineAcceptsNickelWhenInserted() {
-        VendingMachine machine = new VendingMachine();
 
         assertEquals("nickel", machine.getCoinsInserted("nickel"));
     }
+
+
 }
